94ef42d409004a229e907049b71be5168c62c2bd
Elso Silva <esilva45@hotmail.com>
2020-03-11 09:45:50 -0300

    Start

---
+++ ReadFileCustom/TailfParameters.cs
@@ -0,0 +1,13 @@
+﻿
+namespace ReadFileCustom {
+    public class TailfParameters : CommandLineParser {
+        public TailfParameters(string[] args)
+            : base(args) {
+        }
+
+        [Option("n", LongHelp = "Lines to show at the end of the files", Optional = true, ShortHelp = "lines count")]
+        public string NOfLines { get; set; }
+        [Option("f", LongHelp = "Regular expression that match a portion of the line to accept it", Optional = true, ShortHelp = "filter regex")]
+        public string Filter { get; set; }
+    }
+}
